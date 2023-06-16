3e908673fd1b3b1c9ddae42600df944f15b647b1
Timemaster111 <53912018+Timemaster111@users.noreply.github.com>
2023-06-16 14:50:30 +0100

    Create index.js

---
+++ API/index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+
+const centra = require("centra")
+const crypto = require("crypto")
+
+app.post("/webhook/register", (req, res) => {
+    // store ip in database?
+})
+
+
+
+require("dotenv").config()
