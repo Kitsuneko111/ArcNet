80baf4733ad055559b8166568a853935a7a56797
Timemaster111 <53912018+Timemaster111@users.noreply.github.com>
2023-06-20 16:45:16 +0100

    Update diagnose.js

--- ArcNet Bot/commands/utility/diagnose.js
+++ ArcNet Bot/commands/utility/diagnose.js
@@ -2,9 +2,16 @@ const {SlashCommandBuilder} = require("discord.js")
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Replies with Pong!'),
+		.setName('diagnose')
+		.setDescription('Runs a diagnostic on your server to determine how secure your permissions are.'),
+    level: "Owner",
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		await interaction.deferReply({ephemeral: true})
+        /*
+        requirements:
+        maxiumum 3 admin roles
+        ideally only 1
+        no roles above admin other than specific owner ones
+        */
 	},
 };
